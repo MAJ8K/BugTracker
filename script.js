04673cf1c04b8233cbbc3ae55265ce514e64d778
michael <Jenkins4k@gmail.com>
2021-10-26 17:02:54 -0400

    complete mvp bug logger

--- script.js
+++ script.js
@@ -14,25 +14,81 @@
 /////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////
-const authTokens = location.href.split(/#|=|&/);
-console.log(authTokens);
-fetch('https://f11dub94fg.execute-api.us-east-1.amazonaws.com/Production/transactions')
+// use format authentication tokens & get json-Table from db
+const logtemp = document.getElementById('logtemp');
+const logcont = document.getElementsByClassName('logcontain')[0];
+const addModal = document.getElementById('addModal');
+const overlay = document.getElementById('overlay');
+const jwt = location.href.split('#')[1];
+const token = jwt.split(/=|&/)[3];
+console.debug(jwt.split(/=|&/));
 
-// arrange slides next to one another
-function arangeSlides(curr) {
-    for (const i in slides) {
-        slides[i].style.left = 
-            `${trackRect.width * (i - curr)}px`;
+let jtable = undefined;
+if (jwt != undefined){
+
+    fetch('https://f11dub94fg.execute-api.us-east-1.amazonaws.com/Production/',{
+        headers:{
+            'Authorization':token,
+        }
+    })
+    .then(item => {
+        return item.text();
+    })
+    .then(format_site)
+    .catch(console.error);
+}
+
+function format_site(data) {
+    jtable = JSON.parse(data);
+    for (const log of jtable) {
+        color = log[2];
+        const elem = logtemp.cloneNode(true);
+        elem.style.display = 'grid';
+        elem.children[0].innerText = log[0];
+        elem.children[1].innerText = log[1];
+        if (log[2].length == 4)
+            color = '#' + log[2][1] + log[2][1] + log[2][2] + log[2][2] + log[2][3] + log[2][3];
+        elem.children[2].value = color;
+        elem.children[3].innerText = log[3];
+        logcont.appendChild(elem);
     }
-};
-arangeSlides(0)
+}
 
-//right button
-carBtns[1].addEventListener('click',e => {
-    const currentSlide = track.querySelector('.curr');
-    const nextSlide = currentSlide.nextElementSibling;
-    console.log(nextSlide);
-    const moveBy = -trackRect.width;
-    //move to next
-    arangeSlides(1)
-});
+function buttonID(id) {
+    overlay.classList.remove('off');
+    switch (id.alt) {
+        case 'add':
+            addModal.classList.remove('off');
+            break;
+        case 'filter':break;
+        case 'sort':break;
+        case 'save':
+            savedata();
+            overlay.classList.add('off');
+            break;
+    }
+}
+
+function addlog(data) {
+    const elem = logtemp.cloneNode(true);
+    elem.style.display = 'grid';
+    elem.children[0].innerText = data[0].value;
+    elem.children[1].innerText = data[1].value;
+    elem.children[2].value = data[2].value;
+    elem.children[3].innerText = data[3].value;
+    logcont.appendChild(elem);
+    addModal.classList.add('off');
+    overlay.classList.add('off');
+
+    jtable.push([data[0].value,data[1].value,data[2].value,data[3].value])
+}
+
+function savedata() {
+    fetch('https://f11dub94fg.execute-api.us-east-1.amazonaws.com/Production/',{
+        method:'PUT',
+        body:JSON.stringify(jtable),
+        headers:{
+            'Authorization':token,
+        }
+    })
+}
